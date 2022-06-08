8c05ae9f0a89a12f9ce68a71cd0470da31d87f43
Alexey Reznichenko <raleksey2005@gmail.com>
2022-06-08 10:29:18 +0200

    pr

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="layout">
-        <!-- <Preloader /> -->
+        <Preloader />
         <div ref="notificationWindow" id="notificationWindow"></div>
         <Navigation/>
         <nuxt class="on-top" />
