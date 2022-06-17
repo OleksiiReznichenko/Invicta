95324b584f40f3a523ca1de735efcbcb67a7d133
Alexey Reznichenko <raleksey2005@gmail.com>
2022-06-18 01:40:55 +0200

    ff

--- plugins/isPageExist.js
+++
@@ -1,6 +0,0 @@
-export default function ({ route, redirect }) {
-    // IF PAGE WITH THAT URL DOESNT EXIST - REDIRECT ON BROWSE PAGE
-    if (!route.name && route.matched.length === 0) {
-        return redirect('/browse');
-    }
-}
